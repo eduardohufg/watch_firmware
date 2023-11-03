f3d4d2358e1b9d6573ba267f1bc33b0ed5767367
Eduardo Chavez Martin <66572435+eduardohufg@users.noreply.github.com>
2023-11-03 01:04:42 -0600

    Hour implementation

--- ssd.py
+++ ssd.py
@@ -22,7 +22,20 @@ write12 = Write(display, u12)
 write15 = Write(display, u15)
 write20 = Write(display, u20)
 
-def show_battery(bat):
+def show_wifi_status(status,x,y):
+    wifi = bytearray(b'\x00\x00\x00\x00\x1f\xf0x<\xe3\x8e\x8f\xe2883\x98\x0f\xe0\x0c`\x03\x80\x03\x80\x03\x80\x00\x00\x00\x00')
+    wifi_no = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+
+    if status == 1:
+        bwifi = framebuf.FrameBuffer(wifi, 15,15, framebuf.MONO_HLSB)
+        display.blit(bwifi,x,y)
+        display.show()
+    else:
+        bwifi = framebuf.FrameBuffer(wifi_no, 15,15, framebuf.MONO_HLSB)
+        display.blit(bwifi,x,y)
+        display.show()
+
+def show_battery(bat,x,y):
     cien = bytearray(b'\x1f\xff\xff\x00 \x00\x00\x80.\xee\xee\x80n\xee\xee\x80n\xee\xee\x80n\xee\xee\x80n\xee\xee\x80.\xee\xee\x80 \x00\x00\x80\x1f\xff\xff\x00')
     ochenta = bytearray(b'\x1f\xff\xff\x00 \x00\x00\x80 \xee\xee\x80`\xee\xee\x80`\xee\xee\x80`\xee\xee\x80`\xee\xee\x80 \xee\xee\x80 \x00\x00\x80\x1f\xff\xff\x00')
     secenta = bytearray(b'\x1f\xff\xff\x00 \x00\x00\x80 \x0e\xee\x80`\x0e\xee\x80`\x0e\xee\x80`\x0e\xee\x80`\x0e\xee\x80 \x0e\xee\x80 \x00\x00\x80\x1f\xff\xff\x00')
@@ -34,27 +47,108 @@ def show_battery(bat):
     
     if(bat >=80 and bat <=110):
         fb = framebuf.FrameBuffer(porcentage[0], 25,10, framebuf.MONO_HLSB)
-        display.blit(fb,103,0)
+        display.blit(fb,x,y)
         display.show()
     if(bat >=60 and bat <=79):
         fb = framebuf.FrameBuffer(porcentage[1], 25,10, framebuf.MONO_HLSB)
-        display.blit(fb,103,0)
+        display.blit(fb,x,y)
         display.show()
     if(bat >=40 and bat <=59):
         fb = framebuf.FrameBuffer(porcentage[2], 25,10, framebuf.MONO_HLSB)
-        display.blit(fb,103,0)
+        display.blit(fb,x,y)
         display.show()
     if(bat >=20 and bat <=39):
         fb = framebuf.FrameBuffer(porcentage[3], 25,10, framebuf.MONO_HLSB)
-        display.blit(fb,103,0)
+        display.blit(fb,x,y)
         display.show()
     if(bat >=0 and bat <=19):
         fb = framebuf.FrameBuffer(porcentage[4], 25,10, framebuf.MONO_HLSB)
-        display.blit(fb,103,0)
+        display.blit(fb,x,y)
         display.show()
+        
+        
+def show_day(form, size, x, y):
+    list_day = ['Lun.','Mar.','Mie.','Jue.','Vie.','Sab.','Dom.']
+    list_day_tot = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
+    if size == 20:
+        write = Write(display, u20)
+    elif size == 15:
+        write = Write(display, u15)
+    else:
+        write = Write(display, u12)
+    if form ==0:
+        write.text(list_day[RTC().datetime()[3]], x, y)
+    elif form ==1:
+        write.text(list_day_tot[RTC().datetime()[3]], x, y)
+    else:
+        write.text(list_day[RTC().datetime()[3]], x, y)
+    display.show()
     
+    
+    
+def show_hour(form, size, x, y):
+    #display.fill(0)
+    list_num =['00','01','02','03','04','05','06','07','08','09']
+    
+    if size == 20:
+        write = Write(display, u20)
+    elif size == 15:
+        write = Write(display, u15)
+    else:
+        write = Write(display, u12)
+        
+    if form == 0:
+        if (RTC().datetime()[4]==0):
+            write.text('12', x, y)
+            write.text("a.m.", x+39,y)
+        elif (RTC().datetime()[4]<10):
+            write.text(list_num[RTC().datetime()[4]], x, y)
+            write.text("a.m.", x+39,y)
+        elif (RTC().datetime()[4]>12):
+            write.text(str(RTC().datetime()[4]-12), x, y)
+            write.text("p.m.", x+39,y)    
+        else:
+            write.text(str(RTC().datetime()[4]), x, y)
+        write.text(':', x+15, y)
+        if (RTC().datetime()[5]<10):
+            write.text(list_num[RTC().datetime()[5]], x+22, y)
+        else:
+            write.text(str(RTC().datetime()[5]), x+22, y)
+        display.show()
+        
+    elif form == 1:
+        if (RTC().datetime()[4]<10):
+            write.text(list_num[RTC().datetime()[4]], x, y)
+        else:
+            write.text(str(RTC().datetime()[4]), x, y)
+        write.text(':', x+15, y)
+        if (RTC().datetime()[5]<10):
+            write.text(list_num[RTC().datetime()[5]], x+22, y)
+        else:
+            write.text(str(RTC().datetime()[5]), x+22, y)
+        display.show()
+        
+    elif form == 2:
+        if (RTC().datetime()[4]<10):
+            write.text(list_num[RTC().datetime()[4]], x, y)
+        else:
+            write.text(str(RTC().datetime()[4]), x, y)
+        write.text(':', x+15, y)
+        if (RTC().datetime()[5]<10):
+            write.text(list_num[RTC().datetime()[5]], x+22, y)
+        else:
+            write.text(str(RTC().datetime()[5]), x+22, y)
+        write.text(':', x+37, y)
+        if (RTC().datetime()[6]<10):
+            write.text(list_num[RTC().datetime()[6]], x+45, y)
+        else:
+            write.text(str(RTC().datetime()[6]), x+45, y)
+        display.show()     
+    
+    
+
 def show_date(form, size, x, y):
-    display.fill(0)
+    #display.fill(0)
     list_num =['00','01','02','03','04','05','06','07','08','09']
     list_month =['ENE','FEB','MAR','ABR','MAY','JUN','JUL','AGO','SEP','OCT','NOV','DIC']
     
@@ -102,18 +196,27 @@ def show_date(form, size, x, y):
 
 battery = 0
 while(True):
-    show_date(0,15,0,0)
+    show_date(0,15,29,0)
+    show_hour(0,15,0,17)
+    show_day(0,15,0,0)
+    
+    show_wifi_status(1,84,0)
+    
+    
     
     battery = 100
-    show_battery(battery)
+    show_battery(battery,103,2)
     
-    time.sleep(2)
+    time.sleep(1)
+    
+    show_wifi_status(0,84,0)
     
     battery = 19
     
-    show_battery(battery)
+    show_battery(battery,103,2)
     
-    time.sleep(2)
+    
+    #time.sleep(1)
     
     
     
